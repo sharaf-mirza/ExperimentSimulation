2fb5b8ac2e6171daede9655f1191c5706f66221e
sharaf-mirza <Sharaf.Mirza@ruhr-uni-bochum.de>
2026-04-22 12:33:31 +0200

    created Julia Project

---
+++ notebooks/notebook1.jl
@@ -0,0 +1,24 @@
+### A Pluto.jl notebook ###
+# v0.20.24
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e42debc4-3e34-11f1-8733-4d76af361c73
+begin
+	import Pkg
+	Pkg.activate(joinpath(@__DIR__, ".."))
+
+	Pkg.instantiate()
+end
+
+# ╔═╡ 74d91fdb-e6c1-4829-ada1-0f7a67d74097
+using Plots, Random
+
+# ╔═╡ 2447f0ea-3eb7-4e51-b454-11fd79873cc7
+
+
+# ╔═╡ Cell order:
+# ╠═e42debc4-3e34-11f1-8733-4d76af361c73
+# ╠═74d91fdb-e6c1-4829-ada1-0f7a67d74097
+# ╠═2447f0ea-3eb7-4e51-b454-11fd79873cc7
---
+++ src/ExperimentSimulation.jl
@@ -0,0 +1,5 @@
+module ExperimentSimulation
+
+greet() = print("Hello World!")
+
+end # module ExperimentSimulation
